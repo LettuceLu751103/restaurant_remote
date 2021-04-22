347f678a2d2d9ed0661af683778443786b852fd3
lettuce <lettuce.lu@lettucelu.com>
2021-04-22 13:32:29 +0800

    app.js 進行排版及註釋撰寫

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-
+// 引入express 模塊
 const express = require('express')
 const app = express()
+// 服務器監聽端口
 const port = 3000
+// 引入網頁模板 handlebars
+const exphbs = require('express-handlebars')
+// 引入網頁所需資料文件
+const restaurant = require('./restaurant.json')
 
+
+
+
+// 配置靜態文件目錄
 app.use(express.static('public'))
 
-
-
-const exphbs = require('express-handlebars')
+// 配置網頁模板區域
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-const restaurant = require('./restaurant.json')
+
+
+
+
+
+
+// HTTP Request 請求區域
+// 跟目錄請求區域, 若請求 / 成功, 則進行 index.handlebars 渲染, 並將 { restaurants: restaurant.results } 物件傳入
 app.get('/', (req, res) => {
-
-
-  console.log(restaurant.results)
   res.render('index', { restaurants: restaurant.results })
 })
 
+// /restaurants/:id 請求區域, 若請求成功, 則進行 show.handlebars 渲染, 並將 { restaurantOne: restaurantOne[0] } 物件傳入
+// 並且使用 params 參數, 動態獲得參數
 app.get('/restaurants/:id', (req, res) => {
 
   const restaurant_id = req.params.id
+  // 比對所有餐廳清餐, 若客戶端請求id號相同, 則返回該資料
   const restaurantOne = restaurant.results.filter(item => {
     return Number(restaurant_id) === item.id
   })
@@ -29,18 +43,24 @@ app.get('/restaurants/:id', (req, res) => {
   res.render('show', { restaurantOne: restaurantOne[0] })
 })
 
+
+// 搜尋輸入框區域, 若請求 /search 成功, 則進行 index.handlebars 渲染, 並將 { restaurants: newData, keyword: reqData } 物件傳入
 app.get('/search', (req, res) => {
 
-
+  // 客戶端搜尋的字串並且轉換成小寫
   const reqData = req.query.keyword.toLowerCase()
+  // 比對客戶端傳入的字串, 在原先的餐廳清單內是否有符合的字串
   const newData = restaurant.results.filter(item => {
-
     return item.name.toLowerCase().includes(reqData) || item.name_en.toLowerCase().includes(reqData)
   })
   res.render('index', { restaurants: newData, keyword: reqData })
 })
 
 
+
+
+
+// 服務器啟動監聽區域
 app.listen(port, () => {
   console.log(`The Server is running at: http://localhost:${port}`)
 })
