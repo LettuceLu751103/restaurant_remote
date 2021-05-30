afcc8d0cefe67745086b8a84a58becf0e311497e
lettuce <lettuce.lu@lettucelu.com>
2021-05-30 14:27:18 +0800

    feat: add user authentication in restaurants

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 
-
-
 const router = express.Router()
 
 const home = require('./modules/home')
 const other = require('./modules/other')
 const users = require('./modules/users')
+const { authenticator } = require('../middleare/auth')  // 掛載 middleware
 
-router.use('/', home)
-router.use('/restaurants', other)
+
+router.use('/restaurants', authenticator, other)
 router.use('/users', users)
+router.use('/', authenticator, home)
 module.exports = router
